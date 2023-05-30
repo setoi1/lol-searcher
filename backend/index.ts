cdccb5d9e337f8da3df00015ee54cb36ef787b20
Ian Seto <i.seto00@gmail.com>
2023-05-30 18:31:18 -0400

    cleaned up code

--- backend/index.ts
+++ backend/index.ts
@@ -12,13 +12,10 @@ app.use("/api", router);
 
 const PORT: number = parseInt(process.env.EXPRESS_PORT as string, 10);
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
 app.use(bodyParser.json({ limit: "1000mb" }))
 
-// Enable CORS
 app.use(cors());
 
 app.listen(PORT, () => {
